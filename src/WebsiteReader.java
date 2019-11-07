622618f6d50b3ce7d4e18b322cad9e1d16c0a530
Neer Jariwala <njariwala@wpi.edu>
2019-11-07 14:26:46 -0500

    getting lyrics (with section titles) complete

--- src/WebsiteReader.java
+++ src/WebsiteReader.java
@@ -7,10 +7,8 @@ public class WebsiteReader {
     public static void main(String[] args) throws Exception{
         Document doc = Jsoup.connect("http://www.absolutelyrics.com/lyrics/view/conan_gray/idle_town").get();
         doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
-        doc.select("br").append("\\n");
-        doc.select("p").prepend("\\n\\n");
-        String s = doc.html().replaceAll("\\\\n", "\n");
-        Elements lyrics = doc.select("#view_lyrics");
+        doc.select("br").remove();
+        String lyrics = doc.select("#view_lyrics").html();
         System.out.println(lyrics);
     }
 
