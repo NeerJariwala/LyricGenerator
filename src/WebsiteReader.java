845a459fbcf6e749bd3f83e8712e5dc31d5ee80a
Neer Jariwala <njariwala@wpi.edu>
2019-11-07 14:37:00 -0500

    lyrics with no [] and comments

--- src/WebsiteReader.java
+++ src/WebsiteReader.java
@@ -1,14 +1,26 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class WebsiteReader {
 
     public static void main(String[] args) throws Exception{
+
+        //connect to website
         Document doc = Jsoup.connect("http://www.absolutelyrics.com/lyrics/view/conan_gray/idle_town").get();
+
+        //keep break lines
         doc.outputSettings(new Document.OutputSettings().prettyPrint(false));
+
+        //removes break lines from html
         doc.select("br").remove();
+
+        //grab the lyrics from the website
         String lyrics = doc.select("#view_lyrics").html();
+
+        //removes all text between [] and the [] themselves
+        lyrics = lyrics.replaceAll("\\[.*?]", "");
+
+        //print lyrics
         System.out.println(lyrics);
     }
 
