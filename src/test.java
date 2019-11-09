211eef74047e827ff94db53e60a8ff65d8306fc0
wblucca <wblucca@wpi.edu>
2019-11-09 16:35:12 -0500

    Fix JFrame window so button actions happen, but weird layout

--- src/test.java
+++ src/test.java
@@ -1,21 +1,47 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class test extends JFrame implements ActionListener {
 
     private JTextArea output;
-    private JButton Generate = new JButton("Generate");
+    private JButton btnGenerate;
 
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
 
     public static void main(String[] args) {
+        new test(WIDTH, HEIGHT).setVisible(true);
+    }
 
+    private test(int width, int height) {
+        // Parameters for the JFrame window
+        setTitle("Lyric Generator");
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Add the form
+        btnGenerate.addActionListener(this);
+        add(output);
+        add(btnGenerate);
+
+        // Move window to center of screen
+        setLocationRelativeTo(null);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        WebsiteReader result = new WebsiteReader();
-        //output.append(result);
+
+        if (e.getSource() == btnGenerate) {
+            try {
+                System.out.println("Click!");
+                WebsiteReader.main(new String[0]);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
     }
+
 }
